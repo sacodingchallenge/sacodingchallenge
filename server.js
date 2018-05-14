05d18e38428e27197ae785b041bd7fc9fa3e7168
Jacob Tripp <jake@shimmer.home>
2018-05-14 15:37:50 -0500

    Refactor to use async

--- server.js
+++ server.js
@@ -1,104 +1,70 @@
-const express = require('express');
-const ejs = require('ejs');
-const request = require('request');
-const moment = require('moment-timezone');
+const express = require("express");
+const axios = require("axios");
 
-const findNextEvents = require('./js/nextEvent.js');
-const getPhotosArr = require('./js/getPhotos.js');
-
-// ===============================================================
-
-getEventData();
-
-function getEventData() {
-
-	request({
-		url: "https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=San-Antonio-PHP-Meetup&status=upcoming&page=50",
-		json: true
-	}, function(error, response, body) {
-
-		if (!error && response.statusCode === 200) {
-			var data = findNextEvents(body.results);
-			// figure out a better way to do this
-			// problem is the api call is async, and so when you try to pass it in it won't work
-			// app.get('/events') should be with the other app.get's down below
-			// promises?
-			app.get('/events', (req, res) => {
-				res.render('events.ejs', {data: data});
-			});
-		} else {
-			console.log('Unable to fetch data.');
-		}
-
-	});
-
-}
-
-getPhotos();
-
-// kinda a duplicate
-// only difference is url, file name, format function
-// maybe make a function and call it twice with each of these?
-function getPhotos() {
-
-	request({
-		url: "https://api.meetup.com/2/photo_albums?&sign=true&photo-host=public&group_id=18644645&page=20",
-		json: true
-	}, function(error, response, body) {
-
-		if (!error && response.statusCode === 200) {
-			
-			var photos = getPhotosArr(body.results);
-
-			app.get('/photos', (req, res) => {
-				res.render('photos.ejs', {photos: photos});
-			});
-		} else {
-			console.log('Unable to fetch data.');
-		}
-
-	});
-
-}
-
-// ===============================================================
+const findNextEvents = require("./js/nextEvent.js");
+const getPhotosArr = require("./js/getPhotos.js");
 
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/css'));
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/images'));
-app.use(express.static(__dirname + '/font-awesome-4.7.0'));
-app.use(express.static(__dirname + '/fonts'));
-app.use(express.static(__dirname + '/lib'));
+app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/js"));
+app.use(express.static(__dirname + "/images"));
+app.use(express.static(__dirname + "/font-awesome-4.7.0"));
+app.use(express.static(__dirname + "/fonts"));
+app.use(express.static(__dirname + "/lib"));
 
-app.get('/', (req, res) => {
-	res.render('index');
+// ROUTES
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-	res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/sponsors', (req, res) => {
-	res.render('sponsors');
+app.get("/events", async (req, res) => {
+	try {
+		let response = await axios(
+			"https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=San-Antonio-PHP-Meetup&status=upcoming&page=50"
+		);
+		let data = findNextEvents(response.data.results);
+		res.render("events.ejs", { data });
+	} catch (e) {
+		res.status(500).send("Something went wrong!");
+	}
 });
 
-app.get('/help', (req, res) => {
-	res.render('help');
+app.get("/photos", async (req, res) => {
+	try {
+		let response = await axios(
+			"https://api.meetup.com/2/photo_albums?&sign=true&photo-host=public&group_id=18644645&page=20"
+		);
+		let photos = getPhotosArr(response.data.results);
+		res.render("photos.ejs", { photos });
+	} catch (e) {
+		res.status(500).send("Something went wrong!");
+	}
 });
 
-app.get('/subscribe', (req, res) => {
-	res.render('subscribe');
+app.get("/sponsors", (req, res) => {
+  res.render("sponsors");
+});
+
+app.get("/help", (req, res) => {
+  res.render("help");
+});
+
+app.get("/subscribe", (req, res) => {
+  res.render("subscribe");
 });
 
 app.listen(port, () => {
-	console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
-
 
 // { name: '2nd Annual Halloween Coding Challenge',
 //   url: 'https://www.meetup.com/San-Antonio-PHP-Meetup/events/242820588/',
@@ -109,8 +75,8 @@ app.listen(port, () => {
 //   city: 'San Antonio',
 //   state: 'TX' }
 
-// make a api call to 
+// make a api call to
 // https://api.meetup.com/2/photo_albums?&sign=true&photo-host=public&group_id=18644645&page=20
-// use photo_album_id to make another call to 
+// use photo_album_id to make another call to
 // https://api.meetup.com/2/photos?&sign=true&photo-host=public&photo_album_id=28111592&page=20
 // which will give you all of the photos in the album.
